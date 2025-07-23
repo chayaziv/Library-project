561fb85bbaa1f105e04d3e83db3f6fba3621802c
chayaziv <chy15658@gmail.com>
2025-07-23 11:50:05 +0300

    changes in my server

--- server/DAL/PackageUser.cs
+++ server/DAL/PackageUser.cs
@@ -1,9 +1,14 @@
-﻿namespace DAL
+﻿using System.Text.Json.Serialization;
+
+namespace DAL
 {
     public class PackageUser
     {
         public int Id { get; set; }
         public int UserId { get; set; }
+
+        [JsonIgnore]
+       
         public virtual User User { get; set; }
         public int PackageId { get; set; }
         public virtual Package? Package { get; set; }
